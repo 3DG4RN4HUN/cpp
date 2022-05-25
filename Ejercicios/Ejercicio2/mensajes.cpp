9d12def484199801aecd548c52d99be111ad8154
Edgar Dominguez <dominguezedgar809@gmail.com>
2022-05-25 14:27:50 -0600

    agregando ejercicio 2

--- Ejercicios/Ejercicio2/mensajes.cpp
+++ Ejercicios/Ejercicio2/mensajes.cpp
@@ -6,12 +6,12 @@ int main(int argc, char const *argv[])
 {
     string nombre;
 
-    cout << "Ingrese su nombre : "
+    cout << "Ingrese su nombre : ";
     cin >> nombre;
 
     cout << endl;
     cout << "Hola, mi nombre es " << nombre;
     cout << endl;
-    
+
     return 0;
 }
